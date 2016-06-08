08d998793eb48c109bef23368f5cd446087a2e53
NimuKaT <takumin923@gmail.com>
2016-06-09 00:40:09 +1000

    Begin UI

--- UI_handler.py
+++ UI_handler.py
@@ -1,6 +1,22 @@
 import os.path
-from tkinter import *
-class ui_handler:
-	def __init__(self):
-		pass
-	
+import tkinter as tk
+class ui_handler(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.pack
+        self.create_button()
+
+    def create_button(self):
+        new_button = tk.Button(self)
+        new_button["text"] = "Gab is BAD"
+        new_button["command"] = self.aidan
+        new_button.pack(side="top")
+
+        self.QUIT = tk.Button(self, text="QUIT", fg="red", command=root.destroy)
+        self.QUIT.pack(side="bottom")
+
+    def aidan(self):
+        print("Aidan is also bad")
+root = tk.Tk()
+app = ui_handler(master=root)
+app.mainloop()
