82215494410701e6670784d6b7d83c6d552ff87f
NimuKaT <takumin923@gmail.com>
2016-06-09 21:20:10 +1000

    Testing with UI - added more commenting - playing with tkinter

--- UI_handler.py
+++ UI_handler.py
@@ -3,25 +3,49 @@
     Date:       17/05/2016
     
 """
+import Main_engine
 import os.path
 import tkinter as tk
-class ui_handler(tk.Frame):
+
+class ui_handler():
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.pack
+        tk.Wm.title(master, "Aidan is bad")
+        tk.Wm.minsize(master, 300, 400)
+        self.frame = tk.Frame()
+        self.frame.pack()
         self.create_button()
+        self.frame.pack_forget()
+        self.main_menu()
+
 
     def create_button(self):
-        new_button = tk.Button(self)
+        new_button = tk.Button()
         new_button["text"] = "Gab is BAD"
         new_button["command"] = self.aidan
+        new_button["relief"] = "ridge"
+        new_button["state"] = "normal"
+
+        photo = tk.PhotoImage(file="img01.gif")
+        new_button["image"] = photo
+        new_button.image = photo
+
         new_button.pack(side="top")
 
-        self.QUIT = tk.Button(self, text="QUIT", fg="red", command=root.destroy)
+        self.QUIT = tk.Button(text="QUIT", fg="red", command=root.destroy, relief="flat")
         self.QUIT.pack(side="bottom")
+
+    def main_menu(self):
+        main_menu_frame = tk.Frame()
+        start_button = tk.Button(main_menu_frame, text="1")
+        start_button.pack()
+        option_button = tk.Button(main_menu_frame, text="2")
+        exit_button = tk.Button(main_menu_frame, text="3")
+        main_menu_frame.pack() 
 
     def aidan(self):
         print("Aidan is also bad")
+
+
 root = tk.Tk()
 app = ui_handler(master=root)
-app.mainloop()
+root.mainloop()
